43cfcaa4fa8dd3ac611154179771f8daf64fba2a
tinogomes <tinorj@gmail.com>
2017-10-18 21:20:11 -0200

    new aliases: myip, pyserver, and rbserver

--- misc-patch.zsh
+++ misc-patch.zsh
@@ -1,8 +1,9 @@
 alias faggy='say -v Good\ News faggy faggy faggy faggy faggy faggy faggy faggy'
 alias galias='alias | grep'
-alias myip='curl -s curlmyip.com'
+alias myip='dig +short myip.opendns.com @resolver1.opendns.com'
 alias reload="source ~/.zshrc"
-alias simple-http-server="python -m SimpleHTTPServer"
+alias pyserver="python -m SimpleHTTPServer"
+alias rbserver="ruby -run -e httpd ."
 alias ssh-key-copy="cat ~/.ssh/id_rsa.pub | pbcopy"
 alias ssh-list='cat ~/.ssh/config'
 alias traling-spaces="sed -i '' 's/[[:space:]]\{1,\}$//'"
